d31008f025ff5b446d2492ce216a9e08f5eb2cf4
Tanaka Kensaku <tannakaken@gmail.com>
2022-09-18 11:49:52 +0900

    SEOのためにdescriptionを追加

--- pages/CustomHead.tsx
+++ pages/CustomHead.tsx
@@ -20,6 +20,7 @@ const CustomHead = ({
   return (
     <Head>
       <title>{title}</title>
+      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta name="keywords" content={keyword} />
